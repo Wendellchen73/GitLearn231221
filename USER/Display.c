caf6c534828126d16d0663de47bfbc43cbeb7ed8
Wendell <25110441@qq.com>
2023-12-25 15:14:27 +0800

    modify Displayc 01

--- USER/Display.c
+++ USER/Display.c
@@ -4,7 +4,7 @@ uint8_t    GuB_ItemMsk;
 uint8_t    GuB_PwrupdisFlg;
 uint8_t    GuB_DisStatus;//0:Auto display; 1:key:2:Inset user card;
                                            // 3:inset check card
-
+//modify 231225--01
 
 uint16_t GuW_Item;
 uint8_t    GuB_SubItem;
